ad895763b7ac17ef4ff9806d1b2af2b45e57b2a1
JMUCF <ju398578@ucf.edu>
2024-11-06 13:52:01 -0500

    Initial commit

---
+++ Assets/InventoryItem.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InventoryItem
+{
+    public int ID;
+    public string Name;
+    public int Value;
+}
---
+++ Assets/InventoryManager.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InventoryManager : MonoBehaviour
+{
+
+    public void Start()
+    {
+
+    }
+
+
+    public InventoryItem LinearSearchByName(string itemName)
+    {
+
+        return null;
+    }
+
+    public InventoryItem BinarySearchByID(int ID)
+    {
+
+    return null;
+    }
+
+    public InventoryItem QuickSortByValue(int Value)
+    {
+        
+        return null;
+    }
+}
