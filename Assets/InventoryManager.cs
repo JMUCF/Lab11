cef4a0b34889ee401d7ba4361975d3d58ba497f1
JMUCF <ju398578@ucf.edu>
2024-11-06 15:04:25 -0500

    this is broken. fix :)

--- Assets/InventoryManager.cs
+++ Assets/InventoryManager.cs
@@ -1,6 +1,8 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using static UnityEngine.ParticleSystem;
 
 public class InventoryManager : MonoBehaviour
 {
@@ -8,7 +10,7 @@ public class InventoryManager : MonoBehaviour
     public InventoryItem[] InventoryItems;
     private int inventoryLength = 10;
     public int IDToFind;
-    public bool BinarySearch;
+    public bool BinarySearch, QuickSort;
 
     public void Start()
     {
@@ -16,12 +18,12 @@ public class InventoryManager : MonoBehaviour
         for (int i = 0; i < inventoryLength; i++)
         {
             InventoryItems[i] = new InventoryItem();
-            InventoryItems[i].ID = Random.Range(0,100);
+            InventoryItems[i].ID = UnityEngine.Random.Range(0, 100);
             InventoryItems[i].Name = "Cool Item " + InventoryItems[i].ID;
-            InventoryItems[i].Value = Random.Range(0, 100);
-            Debug.Log(InventoryItems[i].Name);
-            Debug.Log(InventoryItems[i].Value);
-            Debug.Log(InventoryItems[i].ID);
+            InventoryItems[i].Value = UnityEngine.Random.Range(0, 100);
+            //Debug.Log(InventoryItems[i].Name);
+            //Debug.Log(InventoryItems[i].Value);
+            //Debug.Log(InventoryItems[i].ID);
         }
         if (BinarySearch)
         {
@@ -29,11 +31,20 @@ public class InventoryManager : MonoBehaviour
             if (IDFound == -1) { Debug.LogWarning("ID Not Within Array"); }
             else { Debug.LogWarning("ID Found at" + IDFound + "In array"); }
         }
+
+        if (QuickSort)
+        {
+            QuickSortByValue(InventoryItems, 0, InventoryItems.Length - 1);
+        }
     }
 
     public InventoryItem LinearSearchByName(string itemName)
     {
-
+        for (int i = 0; i < inventoryLength; i++)
+        {
+            if (InventoryItems[i].Name == itemName)
+                return InventoryItems[i];
+        }
         return null;
     }
 
@@ -54,9 +65,42 @@ public class InventoryManager : MonoBehaviour
         return -1;
     }
 
-    public InventoryItem QuickSortByValue(int Value)
+    public void QuickSortByValue(InventoryItem[] arr, int first, int last)
     {
-        
-        return null;
+        if (first < last)
+        {
+            int pivot = Partition(arr, first, last);
+
+            QuickSortByValue(arr, first, pivot - 1);
+            QuickSortByValue(arr, pivot + 1, last);
+        }
+    }
+
+    public int Partition(InventoryItem[] arr, int first, int last)
+    {
+        int pivot = InventoryItems[last].Value;
+        int smaller = InventoryItems[first].Value;
+
+        for (int element = first; element < last; element++)
+        {
+            Debug.Log("Element is: " + element);
+            Debug.Log("Last is: " + last);
+
+            if (arr[element].Value < pivot)
+            {
+                element++;
+
+                int temporary = arr[smaller].Value;
+                arr[smaller].Value = arr[element].Value;
+                arr[element].Value = temporary;
+            }
+        }
+
+        int temporaryNext = arr[smaller + 1].Value;
+        arr[smaller + 1].Value = arr[last].Value;
+        arr[last].Value = temporaryNext;
+
+        Debug.Log(smaller + 1);
+        return smaller + 1;
     }
 }
