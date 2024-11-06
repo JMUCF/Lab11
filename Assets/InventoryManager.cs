00de64088da3a2fd8cead164eb5f944023a16dd7
JMUCF <ju398578@ucf.edu>
2024-11-06 14:12:15 -0500

    uhh

--- Assets/InventoryManager.cs
+++ Assets/InventoryManager.cs
@@ -5,11 +5,23 @@ using UnityEngine;
 public class InventoryManager : MonoBehaviour
 {
 
+    public InventoryItem[] InventoryItems;
+    private int inventoryLength = 10;
+
     public void Start()
     {
-
+        InventoryItems = new InventoryItem[inventoryLength];
+        for (int i = 0; i < inventoryLength; i++)
+        {
+            InventoryItems[i] = new InventoryItem();
+            InventoryItems[i].ID = Random.Range(0,100);
+            InventoryItems[i].Name = "Cool Item " + InventoryItems[i].ID;
+            InventoryItems[i].Value = Random.Range(0, 100);
+            Debug.Log(InventoryItems[i].Name);
+            Debug.Log(InventoryItems[i].Value);
+            Debug.Log(InventoryItems[i].ID);
+        }
     }
-
 
     public InventoryItem LinearSearchByName(string itemName)
     {
